de31a71ac9e74a87c70099391e333b7d8a0824ca
maksim <pro100maks93@gmail.com>
2022-11-19 14:06:12 +0300

    гиря

---
+++ 006/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int d = 2;
+int f = 6;
+int g = 8;
+int h = 4;
+
+int max = a;
+
+if (a > max) max = a;
+if (d > max) max = d;
+if (f > max) max = f;
+if (g > max) max = g;
+if (h > max) max = h;
+
+Console.Write("max = ");
+Console.Write(max);

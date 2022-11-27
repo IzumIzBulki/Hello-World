dd197d41db396c4124e02104b719bd0b4f0b96c4
maksim <pro100maks93@gmail.com>
2022-11-27 23:51:42 +0300

    DZsem#2

--- TaskUtkinMaksim2/DopDZ/Program.cs
+++ TaskUtkinMaksim2/DopDZ/Program.cs
@@ -6,13 +6,14 @@ int n = Convert.ToInt32(Console.ReadLine());
 int Count2 = 0;
 int Count3 = 0;
 
+if ( n == 0 ) Console.WriteLine("введите минимум 2 цифры");
+else
 while (n != 0)
 {
     Count2 = n;
     n = Convert.ToInt32(Console.ReadLine());
     if ( n > 0) Count3 = Count2;
+    else
+    if (Count3 == 0) Console.WriteLine("введите минимум 2 цифры");
 }
 Console.Write($"Наша цифра: {Count3}");
-
-
-

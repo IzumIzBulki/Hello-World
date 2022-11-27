ec62adf541b3cc6da7730e0efbb1a0423eac7ed2
maksim <pro100maks93@gmail.com>
2022-11-27 23:41:05 +0300

    DZsem2

---
+++ TaskUtkinMaksim2/DopDZ/Program.cs
@@ -0,0 +1,18 @@
+﻿// Дополнительное
+
+
+Console.WriteLine("Вводите значения, если хотите остановить, введите 0");
+int n = Convert.ToInt32(Console.ReadLine());
+int Count2 = 0;
+int Count3 = 0;
+
+while (n != 0)
+{
+    Count2 = n;
+    n = Convert.ToInt32(Console.ReadLine());
+    if ( n > 0) Count3 = Count2;
+}
+Console.Write($"Наша цифра: {Count3}");
+
+
+

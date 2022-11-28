c6c71844097b1da9e8c0af44236b497699fe0ebc
maksim <pro100maks93@gmail.com>
2022-11-28 18:50:40 +0300

    13 доделал

--- TaskUtkinMaksim2/013/Program.cs
+++ TaskUtkinMaksim2/013/Program.cs
@@ -3,13 +3,23 @@ Console.Clear();
 
 
 int n = Convert.ToInt32(Console.ReadLine());
+// if (n < 0) n = n * -1;
+// string s = n.ToString();
 
-string s = n.ToString();
+// int v = Convert.ToInt32((n / Math.Pow(10, s.Length-3))%10);
+// if (v == 0) Console.WriteLine("Введите 3ех значное число");
+// else
+//     Console.WriteLine(v);
 
-int v = Convert.ToInt32((n / Math.Pow(10, s.Length-3))%10);
-if (v == 0) Console.WriteLine("Введите 3ех значное число");
+if (n < 0) n = n * -1;
 else
-    Console.WriteLine(v);
+if (n < 99) 
+    Console.WriteLine("введи");
+else
+while (n > 1000)
+    n /=10;
+
+Console.WriteLine(n % 10);
 
 
   

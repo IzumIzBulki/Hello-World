ad304d547e20f6dbf9c73d17f780724732797187
Уткин Максим Игоревич <353906@in.sohim.by>
2022-11-29 11:36:03 +0300

    Rabota_V.2

--- Rabota/DZ19/Program.cs
+++ Rabota/DZ19/Program.cs
@@ -1,4 +1,4 @@
-﻿// Task 19
+﻿// Task 19 v.1
 
 Console.Clear();
 
@@ -12,22 +12,25 @@ if ((n / 10000) % 10 == n  % 10 && (n / 1000) % 10 == (n/10)  % 10) Console.Writ
 else
     Console.Write("DontMonogam");
 
-// int n1 = 0; int n2 = n%10; int n11 = 0; int n22 = 0;
-// if (n<9999) Console.Write("Введите 5 значное число");
-// else
-// if (n>99999) Console.Write("Введите 5 значное число");
-// else
-// {
-// while (n>9999)
-//     n = n/10;
-//     n1 = n;
-// while (n>99)
-//     n /=10;
-//     n11 = n;
-// while (n>9)
-//     n /=10;
-//     n22 = n;
-// if (n1%10 == n11%10 && n22%10 == n2) Console.Write("monogam");
-// else
-//     Console.Write("DontMonogam");
-// }
+
+// Task 19 v.2
+
+int n1 = 0; int n2 = n%10; int n11 = 0; int n22 = 0;
+if (n<9999) Console.Write("Введите 5 значное число");
+else
+if (n>99999) Console.Write("Введите 5 значное число");
+else
+{
+while (n>9999)
+    n = n/10;
+    n1 = n;
+while (n>99)
+    n /=10;
+    n11 = n;
+while (n>9)
+    n /=10;
+    n22 = n;
+if (n1%10 == n11%10 && n22%10 == n2) Console.Write("monogam");
+else
+    Console.Write("DontMonogam");
+}

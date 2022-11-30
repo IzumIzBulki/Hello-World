931f08f35351e5feebaedc4e0970c159d2d5b0b5
Уткин Максим Игоревич <353906@in.sohim.by>
2022-11-30 13:12:52 +0300

    DopDZsem3

---
+++ Rabota/DOPdz2/Program.cs
@@ -0,0 +1,29 @@
+﻿// DOPdz2
+
+Console.Clear();
+Console.Write("Введите количество кустов: "); 
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 3) 
+{ 
+    Console.Write("Посадите больше 2ух кустов ");
+    return;
+}
+int[] mod = new int[n];
+int count = mod.Length;
+int max = 0;
+int summ = 0;
+Console.Write("Наши ягоды: "); 
+for (int i = 0; i < n; i++)
+{
+    int x = new Random().Next(0, n+1);
+    mod[i] = x;
+    Console.Write(mod[i]);  
+}
+for (int j = 0; j < mod.Length; j++)
+{
+    if (j != 0 && j !=count-1)
+        summ = mod[j-1] + mod[j+1] + mod[j];
+        if (summ > max) max = summ;
+}
+Console.WriteLine(" "); 
+Console.Write($"Максимальное число ягод: {max}");  

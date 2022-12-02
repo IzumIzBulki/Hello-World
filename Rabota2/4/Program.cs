b1408151b30ba60816ba600e5a4dbedcb8f70b7f
Уткин Максим Игоревич <353906@in.sohim.by>
2022-12-02 16:48:51 +0300

    tack4

---
+++ Rabota2/4/Program.cs
@@ -0,0 +1,14 @@
+﻿// tack 4
+Console.Clear();
+Console.Write("Введите число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+int n4 = 0;
+n4 = n1;
+for (int i = 1; i < n2; i++)
+{
+    n1=n1*n4;
+}
+Console.Write("Ответ: ");
+Console.WriteLine($"{n1}");

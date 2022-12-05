0fd07a7edc3743e1acf54c7ec9f018ff768c2a3b
maksim <pro100maks93@gmail.com>
2022-12-05 21:29:03 +0300

    25

---
+++ Rabota2/25/Program.cs
@@ -0,0 +1,13 @@
+﻿// tack 25
+Console.Clear();
+Console.Write("Введите число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+int n4 = n1;
+for (int i = 1; i < n2; i++)
+{
+    n1=n1*n4;
+}
+Console.Write("Ответ: ");
+Console.WriteLine($"{n1}");

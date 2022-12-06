4a4f6f316c70c56b08b24280e59c6df8cb1015a3
maksim <pro100maks93@gmail.com>
2022-12-06 20:01:19 +0300

    lekcia4

---
+++ lekcia4/Program.cs
@@ -0,0 +1,128 @@
+﻿Console.Clear();
+// string[,] table = new string[2, 5];
+// // String.Empty
+// //table[0,0] table[0,1] table[0,2] table[0,3] table[0,4]
+// //table[1,0] table[1,1] table[1,2] table[1,3] table[1,4]
+// table[1, 2] = "slova";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+// }
+
+// int[,] matrix = new int[3, 4]; //matr.GetLength(1) 1 - столбци (тут их 4) matr.GetLength(0) - 0 строчки (их тут 3)
+// void ProntArray(int[,] matr)
+// {
+// for (int rows = 0; rows < matr.GetLength(0); rows++)
+// {
+//     for (int columns = 0; columns < matr.GetLength(1); columns++)
+//     {
+//         Console.Write($"{matr[rows, columns]} ");
+//     }
+//     Console.WriteLine();
+// }
+// }
+// void FillArray(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matr[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
+
+
+// ProntArray(matrix);
+// FillArray(matrix);
+// Console.WriteLine();
+// ProntArray(matrix);
+
+// int[,] pic = new int[,]
+// {
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+//  {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+//  {0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+//  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// };
+
+
+// void PrintImage(int[,] image)
+// {
+// for (int i = 0; i < image.GetLength(0); i++)
+// {
+//     for (int j = 0; j < image.GetLength(1); j++)
+//     {
+//         if (image[i, j] == 0) Console.Write($" ");
+//         else Console.Write($"+");
+//     }
+//     Console.WriteLine();
+// }
+// }
+
+// void FillImage(int row, int col)
+// {
+//     if (pic[row, col] == 0)
+//     {
+//         pic[row, col] = 1;
+//         FillImage(row-1, col);
+//         FillImage(row, col-1);
+//         FillImage(row+1, col);
+//         FillImage(row, col+1);
+//     }
+// }
+
+// PrintImage(pic);
+// FillImage(13, 13);
+// PrintImage(pic);
+
+// double Factorial (double n)
+// {
+//     if (n == 1) return 1;
+//     else return n * Factorial(n-1);
+// }
+// // Console.WriteLine(Factorial(5));
+// for (double i = 1; i < 20; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+// ФИБОНАЧИ
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1)+f(n-2)
+
+// double f(double n)
+// {
+//     if (n == 1 || n == 2) return 1;
+//     else return f(n-1) + f(n-2);
+// }
+
+// for (double i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"f({i}) = {f(i)}");
+// }
+

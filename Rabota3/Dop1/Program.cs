41e1cfc59c6ef1c4f69035368c637c24ae919b98
Уткин Максим Игоревич <353906@in.sohim.by>
2022-12-08 16:54:57 +0300

    dop+

--- Rabota3/Dop1/Program.cs
+++ Rabota3/Dop1/Program.cs
@@ -1,37 +1,54 @@
-﻿// Dop1
+﻿// // Dop1
 
 Console.Clear();
 
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int n1 = n / 2;
+// int n2 = n - n1;
+// int division = 0;
+// Console.WriteLine($"{n2} ^ {n1}");
+// for (int i = 0; i < n / 2 ; i++)
+// {
+//     division = 0;
+//     for (int j = 2; j <= n/2+1; j++)
+//     {
+//         if (n1 % j != 0 && n2 % j != 0  )
+//         {
+//             division++;
+//                 if (division == n/2-1)
+//                 {
+//                     if (n1 == 2 || n1 == 3 || n1 == 5 || n1 % 3 != 0 )
+//                     {
+//                         Console.WriteLine($"{n2} и {n1}");
+//                         return;
+//                     }
+//                 }
+//         }
+            
+//     }
+//     n1++;
+//     n2--;
+// }
+
+
 int n = Convert.ToInt32(Console.ReadLine());
 
-int n1 = n / 2;
-int n2 = n - n1;
-int division = 0;
-Console.WriteLine($"{n2} ^ {n1}");
-for (int i = 0; i < n / 2 ; i++)
+
+
+for (int i = 4; i <= n; i++)  // 10
 {
-    division = 0;
-    for (int j = 2; j <= n/2+1; j++)
+    int count = 0;
+    for (int j = 1; j <= i; j++)
     {
-        if (n1 % j != 0 && n2 % j != 0  )
-        {
-            division++;
-                if (division == n/2-1)
-                {
-                    if (n1 == 2 || n1 == 3 || n1 == 5 || n1 % 3 != 0 )
-                    {
-                        Console.WriteLine($"{n2} и {n1}");
-                        return;
-                    }
-                }
-        }
-            
+        if (i % j == 0)  count++; //1 2 
     }
-    n1++;
-    n2--;
+    if (count <= 2 )
+        {
+            Console.WriteLine($"{i}");
+            return;
+        }
 }
-
-
 
 
 

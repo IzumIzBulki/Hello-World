72019881714a5aa8e2545abe18e8ede206de8271
Уткин Максим Игоревич <353906@in.sohim.by>
2022-12-09 15:57:07 +0300

    rabota3++

--- Rabota3/Task 36/Program.cs
+++ Rabota3/Task 36/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//task 36
+
+Console.Clear();
+
+int n = Convert.ToInt32(Console.ReadLine());
+int[] massiv = new int[n];
+int summ = 0;
+
+for (int i = 0; i <= massiv.Length-1; i++)
+{
+    massiv[i] = new Random().Next(1, n+1);
+    if (i % 2 != 0) summ = summ + massiv[i];
+}
+Console.WriteLine($"[{String.Join(", ", massiv)}]");
+Console.WriteLine(summ);
--- Rabota3/Task 38/Program.cs
+++ Rabota3/Task 38/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//task 36
+
+Console.Clear();
+
+int n = Convert.ToInt32(Console.ReadLine());
+int[] massiv = new int[n];
+int min = n+1;
+int max = 0;
+
+
+for (int i = 0; i <= massiv.Length-1; i++)
+{
+    massiv[i] = new Random().Next(1, n+1);
+    if(min > massiv[i])
+        min = massiv[i];
+    else
+    if(max < massiv[i]) 
+        max = massiv[i];
+}
+
+Console.WriteLine($"[{String.Join(", ", massiv)}]");
+Console.WriteLine(max - min);

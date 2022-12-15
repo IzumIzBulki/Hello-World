9f6f2c85e1f10d952b1aaad410f260415820afee
maksim <pro100maks93@gmail.com>
2022-12-15 21:35:16 +0300

    sem8

---
+++ sem8/Program.cs
@@ -0,0 +1,218 @@
+﻿// //
+
+Console.Clear();
+
+// // task 41
+
+// Console.Clear();
+// int[] numbers = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+// int count = 0;
+// foreach (int element in numbers)
+// {
+//     if (element > 0)
+//         count++;
+// }
+// Console.WriteLine(count);
+
+
+
+// // task 53 замена строк
+
+// void InputMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+//             Console.Write($"{matrix[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+// void SwapFirstLastString(int[,] matrix)
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         int temp = matrix[0, j];
+//         matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+//         matrix[matrix.GetLength(0) - 1, j] = temp;
+//     }
+
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//             Console.Write($"{matrix[i, j]} \t");
+//         Console.WriteLine();
+//     }
+// }
+
+// Console.Clear();
+// Console.Write("Введите размер матрицы: ");
+// int[] coord = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+// int[,] matrix = new int[coord[0], coord[1]];
+// InputMatrix(matrix);
+// Console.WriteLine("\n\n"); // 2 строки
+// SwapFirstLastString(matrix);
+
+
+// // tsk 55 замена троки на стобец
+
+// void InputMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+//             Console.Write($"{matrix[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+// void SwapFirstLastString(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (i < j)
+//             {
+//                 int temp = matrix[i, j];
+//                 matrix[i, j] = matrix[j, i];
+//                 matrix[j, i] = temp;
+//             }
+//             Console.Write($"{matrix[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// Console.Clear();
+// Console.Write("Введите размер матрицы: ");
+// int[] coord = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+
+// while (coord[0] != coord[1])
+// {
+//     Console.Write("Вы ошиблись!\nВведите размер матрицы: ");
+//     coord = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+// }
+
+// int[,] matrix = new int[coord[0], coord[1]];
+// Console.WriteLine("Начальный массив");
+// InputMatrix(matrix);
+// Console.WriteLine("Конечный массив");
+// SwapFirstLastString(matrix);
+
+
+// // Task 57
+
+
+// void InputMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+//             Console.Write($"{matrix[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void SwapFirstLastString(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (i < j)
+//             {
+//                 int temp = matrix[i, j];
+//                 matrix[i, j] = matrix[j, i];
+//                 matrix[j, i] = temp;
+//             }
+//             Console.Write($"{matrix[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// Console.Clear();
+// Console.Write("Введите размер матрицы: ");
+// int[] coord = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+
+// while (coord[0] != coord[1])
+// {
+//     Console.Write("Вы ошиблись!\nВведите размер матрицы: ");
+//     coord = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+// }
+
+// int[,] matrix = new int[coord[0], coord[1]];
+// Console.WriteLine("Начальный массив");
+// InputMatrix(matrix);
+// Console.WriteLine("Конечный массив");
+// SwapFirstLastString(matrix);
+
+
+//task 59
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void SwapFirstLastString(int[,] matrix)
+{
+    int minValue = matrix[0, 0], minRow = 0, minColumn = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < minValue)
+            {
+                minValue = matrix[i, j];
+                minRow = i;
+                minColumn = j;
+            }
+        }
+    }
+    Console.WriteLine($"Минимум {minValue} на позиции({minRow + 1}, {minColumn + 1})");
+    Console.WriteLine();
+    Console.WriteLine("Конечный массив");
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i != minRow && j != minColumn)
+                Console.Write($"{matrix[i, j]} \t");
+        }
+        if (i != minRow)
+            Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] coord = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[coord[0], coord[1]];
+Console.WriteLine("Начальный массив");
+InputMatrix(matrix);
+SwapFirstLastString(matrix);
+
+

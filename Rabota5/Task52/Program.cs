016d519f9c3b10b5ced0082b4b4f2638cca85c95
maksim <pro100maks93@gmail.com>
2022-12-18 13:06:50 +0300

    Rabota5Task52

--- Rabota5/Task52/Program.cs
+++ Rabota5/Task52/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 52
+
+Console.Clear();
+
+int n = new Random().Next(3, 6);
+int m = new Random().Next(3, 6);
+int[,] matrix = new int[n, m];
+double summ = 0;
+
+void FillMatrix(int[,] array)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        
+        for (int f = 0; f < matrix.GetLength(1); f++)
+        {
+            matrix[i, f] = new Random().Next(1, 10);
+            Console.Write(matrix[i, f]);
+            
+        }
+        Console.WriteLine();
+    }
+}
+
+
+double SummArray(int[,] array)
+{
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        summ = 0;
+        for (int f = 0; f < matrix.GetLength(0); f++)
+        {
+            summ = summ + matrix[f, i];
+        }
+        Console.WriteLine($"Сумма вертикали №{i+1}: {Math.Round(summ/matrix.GetLength(0), 2)}");
+    }
+    return summ;
+}
+
+
+
+FillMatrix(matrix);
+Console.WriteLine();
+SummArray(matrix);

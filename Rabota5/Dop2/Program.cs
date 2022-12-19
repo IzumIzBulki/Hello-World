5fcacf7cd9a12b64f0ca7f4ffac7b0d549999ea8
Уткин Максим Игоревич <353906@in.sohim.by>
2022-12-19 10:26:07 +0300

    dop3

--- Rabota5/Dop2/Program.cs
+++ Rabota5/Dop2/Program.cs
@@ -6,74 +6,74 @@ int[,] matrix = new int[4, 10];
 int count = 0;
 int count2 = 0;
 
-    for (int i = 0; i < 1; i++)
+for (int i = 0; i < 1; i++)
+{
+
+    for (int f = 0; f < matrix.GetLength(1); f++)
     {
-
-        for (int f = 0; f < matrix.GetLength(1); f++)
+        if (f == 0)
         {
-            if (f == 0)
-            {
-                matrix[i, f] = i + f + count;
-                Console.Write($"{matrix[i, f]}\t");
+            matrix[i, f] = i + f + count;
+            Console.Write($"{matrix[i, f]}\t");
 
-            }
-            else
-            if (f == 1)
-            {
-                int m = f - 1;
-                matrix[i, f] = i + f + m + count + i;
-                Console.Write($"{matrix[i, f]}\t");
-
-            }
-            else
-            if (f == 2)
-            {
-                int m = f - 1;
-                int n = f - 2;
-                matrix[i, f] = i + f + n + m + count + i + i;
-                Console.Write($"{matrix[i, f]}\t");
-
-            }
-            else
-            {
-                if (f == matrix.GetLength(1)-1) count2 = -i;
-                int m = f - 1;
-                int n = f - 2;
-                int y = f - 3;
-                matrix[i, f] = i + f + n + m + y + count + count2 + i + i + i;
-                Console.Write($"{matrix[i, f]}\t ");
-
-            }
         }
-        count2 = 0;
-        count++;
-        Console.WriteLine();
+        else
+        if (f == 1)
+        {
+            int m = f - 1;
+            matrix[i, f] = i + f + m + count + i;
+            Console.Write($"{matrix[i, f]}\t");
+
+        }
+        else
+        if (f == 2)
+        {
+            int m = f - 1;
+            int n = f - 2;
+            matrix[i, f] = i + f + n + m + count + i + i;
+            Console.Write($"{matrix[i, f]}\t");
+
+        }
+        else
+        {
+            if (f == matrix.GetLength(1) - 1) count2 = -i;
+            int m = f - 1;
+            int n = f - 2;
+            int y = f - 3;
+            matrix[i, f] = i + f + n + m + y + count + count2 + i + i + i;
+            Console.Write($"{matrix[i, f]}\t ");
+
+        }
     }
-    for (int i = 1; i < matrix.GetLength(0); i++)
+    count2 = 0;
+    count++;
+    Console.WriteLine();
+}
+for (int i = 1; i < matrix.GetLength(0); i++)
+{
+
+    for (int f = 0; f < matrix.GetLength(1); f++)
     {
-        
-        for (int f = 0; f < matrix.GetLength(1); f++)
+        if (i == 1 && f == 0)
         {
-            if (i == 1 && f == 0) 
-            {
-            matrix[i, f] = matrix[i-1, f] + 2; 
+            matrix[i, f] = matrix[i - 1, f] + 2;
             Console.Write($"{matrix[i, f]}\t ");
-            }
-            else
-            if (f + 1 < matrix.GetLength(1))
-            {
-            matrix[i, f] = matrix[i-1, f+1] + 1;
-            Console.Write($"{matrix[i, f]}\t ");
-            }
-            if (f + 1 >= matrix.GetLength(1))
-            {
-            matrix[i, f] = matrix[i-1, f] + f-matrix.GetLength(0)-i;
-            Console.Write($"{matrix[i, f]}\t ");
-            }
         }
-        
-        Console.WriteLine();
+        else
+        if (f + 1 < matrix.GetLength(1))
+        {
+            matrix[i, f] = matrix[i - 1, f + 1] + 1;
+            Console.Write($"{matrix[i, f]}\t ");
+        }
+        if (f + 1 >= matrix.GetLength(1))
+        {
+            matrix[i, f] = matrix[i - 1, f] + f - matrix.GetLength(0) - i;
+            Console.Write($"{matrix[i, f]}\t ");
+        }
     }
+
+    Console.WriteLine();
+}
 
 
 
--- Rabota5/Dop3/Program.cs
+++ Rabota5/Dop3/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Dop3
+
+Console.Clear();
+
+Console.Write("Введите количество элементов по вертикали: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество элементов по горизонтали: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Char[,] matrix = new Char[n, m];
+
+
+
+
+// void FillArray(char[,] matrix)
+// {
+// for (int i = 0; i < n; i++) 
+// {
+//     char examination = Convert.ToChar(new Random().Next(0x041, 0x05A));
+//     for (int j = 0; j < i; j++)
+//     {
+//         if (examination == matrix[i, j]) {examination = Convert.ToChar(new Random().Next(0x030, 0x039)); j = 0;}
+//     }
+//     matrix[i, j] = examination;
+// }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void FillMatrix(char[,] array)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int f = 0; f < matrix.GetLength(1); f++)
+        {
+            char examination = Convert.ToChar(new Random().Next(0x042, 0x042));
+            for (int j = 0; j < f; j++)
+            {
+                if (examination == matrix[i, i]) { examination = Convert.ToChar(new Random().Next(0x057, 0x057)); j = 0; }
+            }
+            matrix[i, f] = examination;
+            
+            Console.Write(matrix[i, f]);
+        }
+        Console.WriteLine();
+    }
+}
+
+FillMatrix(matrix);
+
+
+
+
+

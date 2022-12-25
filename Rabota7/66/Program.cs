a6c16bce1bb5821f58664d5ffbf5c534fcdeaef2
maksim <pro100maks93@gmail.com>
2022-12-25 12:02:03 +0300

    Rabota7

---
+++ Rabota7/66/Program.cs
@@ -0,0 +1,20 @@
+﻿//66
+
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n >= m) { Console.Write($"Второе число должно быть больше первого ({n})"); return; }
+
+int summ2 = 0;
+
+int Summ(int n, int m, int summ2)
+{
+    if (n > m) return summ2;
+    { return Summ(n + 1, m, summ2 = summ2 + n); }
+}
+
+Console.Write(Summ(n, m, summ2));
---
+++ Rabota7/68/Program.cs
@@ -0,0 +1,18 @@
+﻿// 68
+Console.Clear();
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+int ackermann(int n, int m)
+{
+    if (n == 0) return m+1;
+    else if (m == 0) return ackermann(n-1, 1);
+    else return ackermann(n-1, ackermann(n, m-1));
+}
+
+Console.Write(ackermann(n, m));
